382e34707ca4652e80eae24ae808679385fb2fb7
Jim Reynolds <reynoldsj@bswift.com>
2023-08-28 13:30:16 -0400

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxNTk1OA/spectral.js?branch_slug=main&token=4c51807e-9780-4779-a4e2-b3e3134f9572";
+export default { extends: ruleset };
